ff88f57f490ad6f02f4c99d20f0b7b39dc5d1c8d
andrew962 <abadia962@gmail.com>
2020-06-01 19:48:42 -0500

    Add Team

---
+++ components/Team.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row id="team">
+    <v-col cols="12" class="text-center pa-0">
+      <h3 style="color:#46DDB0; font-weight: 400;">Team</h3>
+    </v-col>
+    <v-col cols="12" class="text-center pa-0">
+      <h1 style="color:#007BFF;">Developers</h1>
+    </v-col>
+    <v-col cols="6">
+        <v-row>
+            <v-col></v-col>
+            <v-col>
+                <h3>Andrés Abadia</h3>
+            </v-col>
+            <v-col></v-col>
+        </v-row>
+    </v-col>
+    <v-col cols="6"></v-col>
+  </v-row>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,20 @@
   <v-layout column justify-center align-center>
     <back-ground-image />
     <about-us />
+    <team />
   </v-layout>
 </template>
 
 <script>
 import BackGroundImage from '~/components/BackgroundImage.vue'
 import AboutUs from '~/components/AboutUs.vue'
+import Team from '~/components/Team.vue'
 
 export default {
   components: {
     BackGroundImage,
-    AboutUs
+    AboutUs,
+    Team
   }
 }
 </script>
